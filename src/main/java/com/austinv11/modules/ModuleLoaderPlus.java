a683aa65530a1ba77cee0b640e11a5fdaf669e82
austinv11 <austinv11@gmail.com>
2016-06-23 16:18:29 -0400

    The module will now locate IModule implementations and load them

--- src/main/java/com/austinv11/modules/ModuleLoaderPlus.java
+++ src/main/java/com/austinv11/modules/ModuleLoaderPlus.java
@@ -1,12 +1,34 @@
 package com.austinv11.modules;
 
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sx.blah.discord.Discord4J;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.modules.IModule;
+import sx.blah.discord.modules.ModuleLoader;
+
+import java.lang.reflect.Modifier;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ModuleLoaderPlus implements IModule {
 	
 	private volatile boolean isEnabled;
 	private volatile IDiscordClient client;
+	private static final Logger logger = LoggerFactory.getLogger("ModuleLoader+");
+	
+	static {
+		Reflections reflections = new Reflections(Discord4J.class.getClassLoader());
+		Set<Class<? extends IModule>> modules = reflections.getSubTypesOf(IModule.class);
+		Set<Class<? extends IModule>> filteredModules =  modules.stream()
+																.filter(clazz -> 
+																		!Modifier.isAbstract(clazz.getModifiers()) 
+																		&& !Modifier.isInterface(clazz.getModifiers()))
+																.collect(Collectors.toSet());
+		logger.info("{} internal modules located, loading them...", filteredModules.size());
+		filteredModules.forEach(ModuleLoader::addModuleClass);
+	}
 	
 	@Override
 	public boolean enable(IDiscordClient client) {
